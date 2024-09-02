7d5c53d94d408f5aca18e0704354f77e38143c8a
yuta-fukui-823290 <yuta.fukui_823290@glv.co.jp>
2024-09-02 11:34:17 +0900

    feat: 差分を提出する

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <>
-      <p className="dashboard-page">Dashboard Page</p>
-    </>
-  );
-}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,8 +23,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log(pathname);
-  debugger;
   return (
     <>
       {links.map((link) => {
