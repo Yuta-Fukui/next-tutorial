45f0ddac619b992bf11459802537b8269201950d
yuta-fukui-823290 <yuta.fukui_823290@glv.co.jp>
2024-09-03 16:18:15 +0900

    feat: 差分を追加

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { Button } from "@/app/ui/button";
 import { updateInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
 
@@ -18,10 +18,13 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-  }) {
+}) {
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [_state, formAction] = useActionState(
+    updateInvoiceWithId,
+    initialState
+  );
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
